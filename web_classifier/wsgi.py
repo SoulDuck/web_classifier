14d6d8a8e285eec9431f83cfaf70fa6951c36e2f
seongjungkim <plznw4me@naver.com>
2018-06-15 02:48:48 +0900

    allow host all

--- web_classifier/wsgi.py
+++ web_classifier/wsgi.py
@@ -7,9 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os ,sys
+
 
 from django.core.wsgi import get_wsgi_application
+
+path = os.path.abspath(__file__+'/../..')
+
+if path not in sys.path:
+
+    sys.path.append(path)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_classifier.settings")
 
