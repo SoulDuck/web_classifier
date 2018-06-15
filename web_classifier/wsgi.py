eb7c11daee59bda57dc434405f6d7ad92ffc3e3e
seongjungkim <plznw4me@naver.com>
2018-06-15 19:34:37 +0900

    allow host all

--- web_classifier/wsgi.py
+++ web_classifier/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os ,sys
 from django.core.wsgi import get_wsgi_application
 
-path = os.path.abspath('/home/ubuntu/web_classifier')
+path = os.path.abspath(__file__+'/../..')
 if path not in sys.path:
     sys.path.append(path)
 
