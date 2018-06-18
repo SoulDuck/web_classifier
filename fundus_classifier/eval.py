22d2e6209a671a55cdd575d9255ea28be8d28521
kimseongjung <plznw4me@naver.com>
2018-06-18 23:18:18 +0900

    absolute path

--- fundus_classifier/eval.py
+++ fundus_classifier/eval.py
@@ -94,8 +94,8 @@ def diagnose_images(predictions):
 def overlay(actmap , ori_img ,save_path , factor):
     assert factor <= 1 and factor >= 0
     cmap = plt.cm.jet
-    plt.imsave(fname='tmp.png', arr=cmap(actmap))
-    cam_img=Image.open('tmp.png')
+    plt.imsave(fname=os.path.join(save_path,'/home/ubuntu/web_classifier/media/tmp.png'), arr=cmap(actmap))
+    cam_img=Image.open('/home/ubuntu/web_classifier/media/tmp.png')
     #np_cam_img=np.asarray(cam_img).astype('uint8') #img 2 numpy
     ori_img=Image.fromarray(ori_img.astype('uint8')).convert("RGBA")
 
