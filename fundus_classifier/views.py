0c400794d0c6b29f60c16f92b74dec2d4d020c50
kimseongjung <plznw4me@naver.com>
2018-06-19 03:24:57 +0900

    absolute path

--- fundus_classifier/views.py
+++ fundus_classifier/views.py
@@ -35,7 +35,7 @@ def upload_file(request):
             ret_json = []
             #fnames=str(request.FILES['file']) for f in request.FILES.getlist('file'):
             for key in request.FILES:
-                fname=request.FILES[key]
+                fname=str(request.FILES[key])
 
                 img=Image.open(fname)
                 assert img == 3 , "{} {} {} {} {}".format(type(img) , np.shape(img)  ,type(fname) , key , type(key))
