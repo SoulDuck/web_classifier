98796286403c5b5e225aef246d2866c2daff33cf
kimseongjung <plznw4me@naver.com>
2018-06-19 01:11:58 +0900

    absolute path

--- fundus_classifier/views.py
+++ fundus_classifier/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.http import JsonResponse
-from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 import web_classifier.settings as settings
