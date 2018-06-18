c2daf3a53fb1e3478e3352a2e9aab8ef1c213473
kimseongjung <plznw4me@naver.com>
2018-06-19 01:01:48 +0900

    absolute path

--- fundus_classifier/views.py
+++ fundus_classifier/views.py
@@ -35,7 +35,7 @@ def upload_file(request):
             fname=str(request.FILES['file'])
             # load Image
             f_path=os.path.join(settings.MEDIA_ROOT , fname)
-
+            pat_id, exam_date, exam_time = 'None' , 'None' , 'None'
             # dicom check
             if dicom_checker(f_path):
                 pat_id, exam_date, exam_time , img = get_patinfo(f_path)
@@ -62,7 +62,9 @@ def upload_file(request):
             #return render(request, 'show_acc.html',{'value_ret': value_ret, 'value_gla': value_gla, 'value_cat': value_cat})
             #return HttpResponseRedirect({'value_ret': value_ret, 'value_gla': value_gla, 'value_cat': value_cat})
 
-            ret_values=json.dumps({'value_ret': str(value_ret), 'value_gla': str(value_gla), 'value_cat': str(value_cat)})
+            ret_values = json.dumps(
+                {'value_ret': str(value_ret), 'value_gla': str(value_gla), 'value_cat': str(value_cat), 'LR': LR,
+                 'actmap_path': actmap_path ,'patient_id':pat_id, 'exam_date' :exam_date, 'exam_time':exam_time })
             return HttpResponse(ret_values,
                                 content_type="application/json")
     else:
