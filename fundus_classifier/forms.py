0fe60219621a460c2b26a7e6d448b6fdf00361b4
kimseongjung <plznw4me@naver.com>
2018-06-19 14:22:25 +0900

    absolute path

--- fundus_classifier/forms.py
+++ fundus_classifier/forms.py
@@ -4,8 +4,7 @@ from models import *
 class UploadForm(ModelForm):
     class Meta:
         model = UploadFile
-
-        #fields = ['file']
+        fields = ['file']
     def __init__(self , *args , **kwargs):
         super(ModelForm , self).__init__(*args, **kwargs)
         self.fields['file'].required = False
