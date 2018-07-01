967e2732ca25ffc2bd47fdbd12047c02fa9394a5
kimseongjung <plznw4me@naver.com>
2018-07-01 17:19:13 +0900

    if image size is (800,800 ) up , downsize to 800,800

--- fundus_classifier/views.py
+++ fundus_classifier/views.py
@@ -85,9 +85,10 @@ def upload_file(request):
 
                 # Crop and Resize Numpy Image , Original Image
                 np_cropped_ori_img =  np.asarray(ori_cropped_img).reshape([1]+list(np.shape(ori_cropped_img)))
+                print np.shape(np_cropped_ori_img)
                 if np.shape(np_cropped_ori_img)[0] > 800 :
                     np_cropped_ori_img=Image.fromarray(np_cropped_ori_img.astype('uint8')).resize((800,800) , Image.ANTIALIAS)
-                    print np.shape(np_cropped_ori_img)
+                    print
                     print np.max(np_cropped_ori_img)
 
                 # Resized Image by (300 x 300)
